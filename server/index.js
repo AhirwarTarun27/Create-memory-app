4adaaffdcbe23c0674740228c65b69c8aac9b71f
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-03-20 00:48:21 +0530

    added env example

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 // cors for enabiling cross origin request
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -16,15 +18,15 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://tarunMemory:memory123@memory2.eopjw.mongodb.net/TarunMemories?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://tarunMemory:memory123@memory2.eopjw.mongodb.net/TarunMemories?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   )
