93be73f188084c48fd3f2bab6154c25fc8f2d820
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-03-26 17:31:07 +0530

    did changes in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
-import memories from "./images/memory.png";
 import useStyles from "./styles";
 import { Form } from "./components/Form/Form";
 import { Posts } from "./components/Posts/Posts";
 import { getPosts } from "./actions/posts";
+import { Navbar } from "./components/Navbar.js/Navbar";
 
 export default function App() {
   const [currentId, setCurrentId] = useState(null);
@@ -19,17 +19,7 @@ export default function App() {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt="memories"
-          height="60"
-        />
-      </AppBar>
+      <Navbar />
       <Grow in>
         <Container>
           <Grid
